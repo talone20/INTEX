cb54bb613afb860a5a4fcbe5e858c78a2b6fc592
Bennett McCullough <mccullough.bennett@gmail.com>
2022-11-29 09:56:46 -0700

    registered classes in admin.py

--- trackme/admin.py
+++ trackme/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Morbidity, Stage, Person, LabReport, JournalEntry
 
 # Register your models here.
+admin.site.register(Morbidity)
+admin.site.register(Stage)
+admin.site.register({Person})
+admin.site.register(LabReport)
+admin.site.register(JournalEntry)
